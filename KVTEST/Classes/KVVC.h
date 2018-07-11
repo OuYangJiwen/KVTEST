513e9aa6274f756325f1c7049aa75d2694360852
mac <mac@macdeMacBook-Pro-2.local>
2018-07-11 18:25:10 +0800

     xxx

---
+++ KVTEST/Classes/KVVC.h
@@ -0,0 +1,14 @@
+//
+//  KVVC.h
+//  KevinPodTest
+//
+//  Created by mac on 2018/7/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVVC : UIViewController
+
++ (void)test;
+
+@end
---
+++ KVTEST/Classes/KVVC.m
@@ -0,0 +1,40 @@
+//
+//  KVVC.m
+//  KevinPodTest
+//
+//  Created by mac on 2018/7/11.
+//
+
+#import "KVVC.h"
+
+@interface KVVC ()
+
+@end
+
+@implementation KVVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
++ (void)test {
+    NSLog(@"这个是测试的");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
