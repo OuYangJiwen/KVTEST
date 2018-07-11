f79fe67b4a016b8f9ba3ceb24c03944a7834a4e2
mac <mac@macdeMacBook-Pro-2.local>
2018-07-11 18:24:47 +0800

     xxx

---
+++ Example/Pods/Target Support Files/KVTEST/KVTEST-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KVTEST : NSObject
+@end
+@implementation PodsDummy_KVTEST
+@end
---
+++ Example/Pods/Target Support Files/Pods-KVTEST_Example/Pods-KVTEST_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KVTEST_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KVTEST_Example
+@end
